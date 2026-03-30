affc6fff95ec3a8d2b217b1199681f5c188f52f6
Young Gyu Park <ygpark2@gmail.com>
2026-03-31 00:29:27 +0900

    ingest: change default port to 8082 to avoid conflicts

--- services/ingestion-service/src/main/scala/com/ainsoft/brain/ingest/Main.scala
+++ services/ingestion-service/src/main/scala/com/ainsoft/brain/ingest/Main.scala
@@ -49,7 +49,7 @@ object Main extends DefaultJsonProtocol {
         }
 
     val bindingF =
-      http.newServerAt("0.0.0.0", 8080).bind(route)
+      http.newServerAt("0.0.0.0", 8082).bind(route)
 
     bindingF.onComplete {
       case Success(binding) =>
